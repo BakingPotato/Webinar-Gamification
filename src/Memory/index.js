ede614de503cb3600c7f67000b31b96c7e826988
Jorge Chavero Morcillo <jchm@INTRASOL.local>
2021-05-09 15:45:25 +0200

    Añadidas funciones al perfil de administrador

--- src/Memory/index.js
+++ src/Memory/index.js
@@ -3,7 +3,7 @@ const IDriver = require('./IDriver');
 
 class databaseConnect extends IDriver {
 
-    async  getSeminariosActivos(){
+    async getSeminariosActivos(){
         const request = new pool.Request();
         const result = await request.execute('OBTENER_SEMINARIOS_ACTIVOS')
         return result.recordset;
@@ -31,6 +31,48 @@ class databaseConnect extends IDriver {
         req.session.usuario = {}; req.session.usuario = result.recordset[0];
         return result.returnValue;
     }
+
+    async registrarSeminario(req){
+        const request = new pool.Request();
+        const result = await request
+        .input("CD_USUARIO", pool.Int, req.session.usuario.CD_USUARIO)
+        .input("DS_NOMBRE", pool.VarChar(50), req.body.DS_NOMBRE)
+        .input("DS_PASS", pool.Int, req.body.DS_PASS)
+        .input("FECHA", pool.Date,  req.body.FECHA)
+        .input("DS_DESCRIPCION", pool.VarChar(250),  req.body.DS_DESCRIPCION)
+        .input("DS_HASHTAG", pool.VarChar(150), null)
+        .execute('REGISTRAR_SEMINARIO')
+        return result.returnValue;
+    }
+
+    async ObtenerSusSeminarios(req){
+        const request = new pool.Request();
+        const result = await request
+        .input("CD_USUARIO", pool.Int, req.session.usuario.CD_USUARIO)
+        .input("DS_CORREO", pool.VarChar(50), req.body.DS_CORREO ? req.body.DS_CORREO : null)
+        .input("DS_NOMBRE", pool.VarChar(50),  req.body.DS_NOMBRE ?  req.body.DS_NOMBRE : null)
+        .input("DS_DESCRIPCION", pool.VarChar(250),  req.body.DS_DESCRIPCION ? req.body.DS_DESCRIPCION : null)
+        .input("DS_PASS", pool.VarChar(50),  req.body.DS_PASS ?  req.body.DS_PASS : null)
+        .input("DS_TWITTER", pool.VarChar(50),  req.body.DS_TWITTER ?  req.body.DS_TWITTER : null)
+        .execute('ACTUALIZAR_USUARIO')
+        req.session.usuario = {}; req.session.usuario = result.recordset[0];
+        return result.returnValue;
+    }
+
+    async ActualizarSeminario(req){
+        const request = new pool.Request();
+        const result = await request
+        .input("CD_USUARIO", pool.Int, req.session.usuario.CD_USUARIO)
+        .input("DS_CORREO", pool.VarChar(50), req.body.DS_CORREO ? req.body.DS_CORREO : null)
+        .input("DS_NOMBRE", pool.VarChar(50),  req.body.DS_NOMBRE ?  req.body.DS_NOMBRE : null)
+        .input("DS_DESCRIPCION", pool.VarChar(250),  req.body.DS_DESCRIPCION ? req.body.DS_DESCRIPCION : null)
+        .input("DS_PASS", pool.VarChar(50),  req.body.DS_PASS ?  req.body.DS_PASS : null)
+        .input("DS_TWITTER", pool.VarChar(50),  req.body.DS_TWITTER ?  req.body.DS_TWITTER : null)
+        .execute('ACTUALIZAR_USUARIO')
+        req.session.usuario = {}; req.session.usuario = result.recordset[0];
+        return result.returnValue;
+    }
+    
 }
 
 module.exports = databaseConnect; 
--- src/routes/perfil.js
+++ src/routes/perfil.js
@@ -3,19 +3,14 @@ const router = express.Router();
 const dbConnect = require('../Memory')
 const { isLoggedIn } = require('../lib/auth');
 
+//Menu de usuario
 router.get('/perfil', isLoggedIn, async (req, res) => {
-    let seminarios = {};
-    if(!req.session.seminarios){
-        seminarios = await dbConnect.prototype.getSeminariosActivos();
-        req.session.seminarios = {};
-        for(let i in seminarios){
-            req.session.seminarios[seminarios[i].CD_SEMINARIO] = seminarios[i];
-        }
+    if(req.session.usuario.ES_ADMIN == 1){
+        res.redirect('/PerfilA')
+    }else{
+        let seminarios = await getSeminarios(req);
+        res.render('menu/perfil', {seminarios});
     }
-    else{
-        seminarios = req.session.seminarios;
-    }
-    res.render('menu/perfil', {seminarios});
 });
 
 router.get('/perfil/registro/:id', isLoggedIn, (req, res) => {
@@ -37,6 +32,48 @@ router.post('/perfil/registro/:id', isLoggedIn, async (req, res) => {
 router.post('/perfil/actualizarPerfil', isLoggedIn, async (req, res) => {
     const newUser = await dbConnect.prototype.actualizarUsuario(req);
     req.flash('success', 'Su usuario se actualizo correctamente');
-    res.redirect('/perfil');});
+    if(req.session.usuario.ES_ADMIN == 0){
+        res.redirect('/perfil');
+    }else{
+        res.redirect('/PerfilA');
+    }
+});
+
+//Menu de administrador
+router.get('/PerfilA', isLoggedIn, async (req, res) => {
+    if(req.session.usuario.ES_ADMIN == 0){
+        res.redirect('/perfil')
+    }
+    let seminarios = await getSeminarios(req);
+    res.render('menu/perfilA', {seminarios});
+});
+
+router.post('/PerfilA/registrarSeminario', isLoggedIn, async (req, res) => {
+    const newSeminario = await dbConnect.prototype.registrarSeminario(req);
+    if(newSeminario == 10000){ //Si ocurrio un error durante el registro avisamos
+        req.flash('message', 'El seminario no pudo registrarse');
+        res.redirect('/PerfilA');
+    }else{ //Borramos los seminarios para que se vuelva a comprobar los seminarios activos
+        req.session.seminarios = null;
+        req.flash('success', 'Seminario registrado correctamente');
+        res.redirect('/PerfilA');
+    }
+   
+});
+
+async function getSeminarios(req){
+    let seminarios = {};
+    if(!req.session.seminarios){
+        seminarios = await dbConnect.prototype.getSeminariosActivos();
+        req.session.seminarios = {};
+        for(let i in seminarios){
+            req.session.seminarios[seminarios[i].CD_SEMINARIO] = seminarios[i];
+        }
+    }
+    else{
+        seminarios = req.session.seminarios;
+    }
+    return seminarios;
+}
 
 module.exports = router;
